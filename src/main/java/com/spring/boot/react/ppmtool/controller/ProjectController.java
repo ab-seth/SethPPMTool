3b5bdc6c01f85dfd88a34ba18b7743a15c4aac95
ABAYO Seth <sethabayo@gmail.com>
2020-04-22 17:18:56 +0200

    adding validation part 2

--- src/main/java/com/spring/boot/react/ppmtool/controller/ProjectController.java
+++ src/main/java/com/spring/boot/react/ppmtool/controller/ProjectController.java
@@ -1,8 +1,11 @@
 package com.spring.boot.react.ppmtool.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +20,12 @@ public class ProjectController {
 	@Autowired
 	private ProjectService projectService;
 	@PostMapping("")
-	public ResponseEntity<Project> createNewProject(@RequestBody Project project){
+	public ResponseEntity<?> createNewProject(
+			@Valid @RequestBody Project project, BindingResult result){
+		
+		if(result.hasErrors()) {
+			return new ResponseEntity<String>("Invalid Project Object",HttpStatus.BAD_REQUEST);
+		}
 		Project theProject = projectService.saveOrUpdate(project);
 		return new ResponseEntity<Project>(project,HttpStatus.CREATED);
 	}
