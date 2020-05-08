a6ddd02be1a182d1717ff834d7690ec1cfb0a0d4
ABAYO Seth <sethabayo@gmail.com>
2020-05-08 16:18:52 +0200

    Project Header component and navbar

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import Dashboard from "./Components/Dashboard";
+import Header from "./Components/Layout/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Dashboard />
     </div>
   );
