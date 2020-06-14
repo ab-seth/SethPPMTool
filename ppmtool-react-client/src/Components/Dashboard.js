1154431b0d218f0fe7c7809140117dfccff3ac68
ABAYO Seth <sethabayo@gmail.com>
2020-06-14 20:10:34 +0200

    React router,first functional component

--- ppmtool-react-client/src/Components/Dashboard.js
+++ ppmtool-react-client/src/Components/Dashboard.js
@@ -1,33 +1,25 @@
 import React, { Component } from "react";
 import ProjectItem from "./project/ProjectItem";
+import CreateProjectButton from "./project/CreateProjectButton";
 
 class Dashboard extends Component {
   render() {
     return (
-     
-
-    <div className="projects">
-    <div class="container">
-        <div class="row">
+      <div className="projects">
+        <div class="container">
+          <div class="row">
             <div class="col-md-12">
-                <h1 class="display-4 text-center">Projects</h1>
-                <br />
-                <a href="ProjectForm.html" class="btn btn-lg btn-info">
-                    Create a Project
-                </a>
-                <br />
-                <hr />
+              <h1 class="display-4 text-center">Projects</h1>
+              <br />
+              <CreateProjectButton />
+              <br />
+              <hr />
 
-                
-                <ProjectItem />
-               
-
+              <ProjectItem />
             </div>
+          </div>
         </div>
-    </div>
-</div>
-
-
+      </div>
     );
   }
 }
