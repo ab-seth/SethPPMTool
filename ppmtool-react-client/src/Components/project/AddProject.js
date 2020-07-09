ab9dd32c4cc670e5a66f7134d3282ffec8e9ded2
ABAYO Seth <sethabayo@gmail.com>
2020-07-09 21:34:00 +0200

    Get validation errors from Redux

--- ppmtool-react-client/src/Components/project/AddProject.js
+++ ppmtool-react-client/src/Components/project/AddProject.js
@@ -12,9 +12,17 @@ class AddProject extends Component {
       description: "",
       start_date: "",
       end_date: "",
+      errors: {},
     };
     this.onChange = this.onChange.bind(this);
     this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  //lifecycle hooks
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.errors) {
+      this.setState({ errors: nextProps.errors });
+    }
   }
 
   onChange(e) {
@@ -30,9 +38,10 @@ class AddProject extends Component {
       start_date: this.state.start_date,
       end_date: this.state.end_date,
     };
-    this.props.createProject(newProject,this.props.history)
+    this.props.createProject(newProject, this.props.history);
   }
   render() {
+    const { errors } = this.state;
     return (
       <div>
         {}
@@ -52,6 +61,7 @@ class AddProject extends Component {
                       value={this.state.projectName}
                       onChange={this.onChange}
                     />
+                    <p>{errors.projectName}</p>
                   </div>
                   <div className="form-group">
                     <input
@@ -62,6 +72,7 @@ class AddProject extends Component {
                       value={this.state.projectIdentifier}
                       onChange={this.onChange}
                     />
+                    <p>{errors.projectIdentifier}</p>
                   </div>
 
                   <div className="form-group">
@@ -71,7 +82,8 @@ class AddProject extends Component {
                       name="description"
                       value={this.state.description}
                       onChange={this.onChange}
-                    ></textarea>
+                    />
+                    <p>{errors.description}</p>
                   </div>
                   <h6>Start Date</h6>
                   <div className="form-group">
@@ -108,7 +120,13 @@ class AddProject extends Component {
   }
 }
 AddProject.propTypes = {
+  createProject: propTypes.func.isRequired,
+  errors: propTypes.object.isRequired
+};
 
-  createProject : propTypes.func.isRequired
-}
-export default connect(null, { createProject })(AddProject);
+const mapStateToProps = state => ({
+  errors: state.errors
+});
+export default connect(mapStateToProps,
+   { createProject }
+   )(AddProject);
